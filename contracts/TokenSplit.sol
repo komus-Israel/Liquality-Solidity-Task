92f3913005f9e3fc5e22373654f7ef7918b4db1b
komus-Israel <komolehinisrael@gmail.com>
2022-04-12 19:47:54 +0100

    added logic to deposit ether in the depositEther function

--- contracts/TokenSplit.sol
+++ contracts/TokenSplit.sol
@@ -12,6 +12,10 @@ import "./utils/IERC20.sol";
 
 contract TokenSplit {
 
+    fallback () external payable {
+
+    }
+
     /// @dev mapping to store token shares allocated to an address
 
     mapping(address => mapping(address => uint256)) private _shares;   
@@ -26,6 +30,10 @@ contract TokenSplit {
     address private splitter;
 
 
+    address private _contractAddress;
+    address private _etherAddress;
+
+
     modifier onlySplitter() {
 
         require(msg.sender == splitter, "invalid sender");
@@ -37,20 +45,28 @@ contract TokenSplit {
     constructor (address _splitter) {
 
         splitter = _splitter;
+        _contractAddress = address(this);
+        _etherAddress = address(0);
 
     }
 
+    /// @param  _token is the address of the ERC20 token that will be deposited
+    /// @param  _amount is the amount of tokens to be deposited by the splitter
+    /// @notice IERC20 _tokenToDeposit = IERC20(_token). Creates an instance of the method of the token contract
+    /// @notice _tokenToDeposit.transferFrom. This contract must have been approved for successful function call
 
     function depositToken(address _token, uint256 _amount) external onlySplitter {
 
         IERC20 _tokenToDeposit = IERC20(_token);
         _tokenToDeposit.transferFrom(msg.sender, address(this), _amount);
-        _tokenBalances[address(this)[_token]] += _amount;
+        _tokenBalances[_contractAddress[_token]] += _amount;
 
     }
 
-    function depositEther() external onlySplitter {
+    function depositEther() external payable onlySplitter {
 
+        _tokenBalances[_contractAddress[_etherAddress]] += msg.value;
+        
     }
 
 }
