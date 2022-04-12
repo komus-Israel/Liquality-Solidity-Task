5e19dd9e6d860c0b148f026332d9e229039769a1
komus-Israel <komolehinisrael@gmail.com>
2022-04-12 19:22:57 +0100

    created a map that stores the value of shares allocated to addreses

--- contracts/TokenSplit.sol
+++ contracts/TokenSplit.sol
@@ -3,6 +3,10 @@ pragma solidity 0.8.13;
 
 contract TokenSplit {
 
-        
+    /// @dev mapping to store shares allocated to addresses
 
+    mapping(address => uint256) private _shares;         
+
+
+    
 }
