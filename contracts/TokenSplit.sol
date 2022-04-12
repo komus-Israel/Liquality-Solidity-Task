7c0d5342e759c59944e3d145dd63e1572d95c6b0
komus-Israel <komolehinisrael@gmail.com>
2022-04-12 19:26:31 +0100

    initialized functions that enables an issuer to deposit ether and erc20 tokens

--- contracts/TokenSplit.sol
+++ contracts/TokenSplit.sol
@@ -1,5 +1,12 @@
 pragma solidity 0.8.13;
 
+/// @title  Token Split Contract
+
+/*  @dev    This is a  smart contract that split ETHER and ERC 20 tokens to
+    *       addresses based on their shares allocation
+
+*/     
+
 
 contract TokenSplit {
 
@@ -8,5 +15,12 @@ contract TokenSplit {
     mapping(address => uint256) private _shares;         
 
 
-    
+    function depositToken(address _token) external {
+
+    }
+
+    function depositEther() external {
+
+    }
+
 }
