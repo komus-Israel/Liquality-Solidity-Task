b6fae112e6c80c50e9ef641a5b28abf36c49a246
komus-Israel <komolehinisrael@gmail.com>
2022-04-12 19:29:40 +0100

    created a map that stores the token balances in the contract

--- contracts/TokenSplit.sol
+++ contracts/TokenSplit.sol
@@ -2,18 +2,24 @@ pragma solidity 0.8.13;
 
 /// @title  Token Split Contract
 
-/*  @dev    This is a  smart contract that split ETHER and ERC 20 tokens to
+/*  
+    *       @dev    This is a  smart contract that split ETHER and ERC 20 tokens to
     *       addresses based on their shares allocation
-
 */     
 
+
+import "./utils/IERC20.sol";
 
 contract TokenSplit {
 
     /// @dev mapping to store shares allocated to addresses
 
-    mapping(address => uint256) private _shares;         
+    mapping(address => uint256) private _shares;   
 
+
+    /// @dev mapping the store the ether and erc20 token balances      
+
+    mapping(address => mapping(address => uint256)) private _tokenBalances;
 
     function depositToken(address _token) external {
 
