dd4117088802ed16ea7ba7cf14964b6166d2c26a
komus-Israel <komolehinisrael@gmail.com>
2022-04-13 20:23:51 +0100

    test contract deployment and deposits in the moneyStream test script

--- test/MoneyStream.test.js
+++ test/MoneyStream.test.js
@@ -36,6 +36,60 @@ contract ("Payment Splitting Unit Test", ([splitter, recipient1, recipient2, rec
         })
 
     })
+
+
+    describe("deposit, splitting and withdrawal", ()=>{
+
+        let etherDeposit
+        let usdtDeposit 
+
+
+        /*
+         * Approve the payment contract for the ERC20 deposit
+         * Deposit ether to the payment contract
+         * Deposit usdt to the payment contract
+        */
+
+        beforeEach(async()=>{
+
+            await usdt.approve(paymentContract.address, tokens(30))
+            etherDeposit = await paymentContract.depositEther({from:splitter, value: ether(20)})
+            usdtDeposit = await paymentContract.depositToken(usdt.address, tokens(30))
+        })
+
+        describe("successful deposits", ()=>{
+
+            it("emits the deposit event", ()=>{
+
+                etherDeposit.logs[0].event.should.be.equal("Deposit", "Deposit event was emitted for ether deposit")
+                usdtDeposit.logs[0].event.should.be.equal("Deposit", "Deposit event was emitted for erc20 token deposit")
+            })
+        
+            it("should not have 0 as the contract's ether and token balance", async()=>{
+                
+                const contractTokenBalance = await usdt.balanceOf(paymentContract.address)
+                const contractEtherBalance = await web3.eth.getBalance(paymentContract.address)
+
+                contractEtherBalance.toString().should.not.be.equal("0", "ether balance of the payment is not equal to zero")
+                contractTokenBalance.toString().should.not.be.equal("0", "token balance of the payment is not equal to zero")
+
+
+            })
+
+
+            it("should not have zero deposits", async()=>{
+
+                const contractDepositedTokenBalance = await paymentContract.getBalance(paymentContract.address, ETHER_ADDRESS)
+                const contractDepositedEtherBalance = await paymentContract.getBalance(paymentContract.address, usdt.address)
+
+                contractDepositedEtherBalance.toString().should.not.be.equal("0", "deposited ether balance of the contract is not zero")
+                contractDepositedTokenBalance.toString().should.not.be.equal("0", "deposited token balance of the contract is not zero")
+
+            })
+            
+        })
+
+    })
    
 
 })
