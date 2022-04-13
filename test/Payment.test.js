30771fbf5bf04a977d74870d256da6fcbae95f19
komus-Israel <komolehinisrael@gmail.com>
2022-04-13 07:30:18 +0100

    initialized test for deposit, splitting and withdrawal

--- test/Payment.test.js
+++ test/Payment.test.js
@@ -7,7 +7,7 @@ const PaymentContract = artifacts.require("./Payment")
 const ERC20_USDT    = artifacts.require("./ERC20")
 
 
-contract ("Payment Splitting Unit Test", ()=>{
+contract ("Payment Splitting Unit Test", ([splitter, recipient1, recipient2, recipient3, recipient4])=>{
 
     let paymentContract;
     let usdt;
@@ -20,11 +20,25 @@ contract ("Payment Splitting Unit Test", ()=>{
 
     })
 
+
+    /*
+        * Test case for the contract addresses
+    */
+
     describe("contract deployment", ()=>{
 
         it("should have a contract address", ()=>{
             paymentContract.address.should.not.be.equal("", "payment contract has an address")
             usdt.address.should.not.be.equal("", "erc20 contract for has an address")
+        })
+
+    })
+
+
+    describe("deposit, splitting and withdrawal", ()=>{
+
+        beforeEach(async()=>{
+
         })
 
     })
