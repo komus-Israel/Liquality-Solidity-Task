86232149395836a36b99e1b6de61e48cbc70fca5
komus-Israel <komolehinisrael@gmail.com>
2022-04-13 07:41:21 +0100

    created an helper file

---
+++ test/helper.js
@@ -0,0 +1,15 @@
+const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+const tokens=(n)=>{
+    return new web3.utils.BN(
+        web3.utils.toWei(n.toString(), 'ether')
+    )
+    
+}
+
+const ether = (n) => tokens(n)
+
+
+
+module.exports = { ETHER_ADDRESS, tokens, ether }
+
